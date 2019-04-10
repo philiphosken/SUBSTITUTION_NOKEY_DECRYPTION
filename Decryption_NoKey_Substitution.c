f0b40976b11a10c903999a13009a1b65cb70a4b3
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-10 10:02:37 +0000

    Rough task ay

--- Decryption_NoKey_Substitution.c
+++ Decryption_NoKey_Substitution.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <ctype.h>
 
+/* int isuppercas(int c) etc etc */
+
+#include <string.h> /*allows comparison of strings*/
+//(strcmp(a,b) == 0)
+
+int main()
+{
+unsigned char mess[1024];
+
+int d[];
+
+
+
+FILE *in;
+in = fopen("in.txt", "r");
+
+FILE *out;
+out = fopen("out.txt", "w");
+
+FILE *diction
+diction = fopen("words.txt", "r");
+    
+    while (feof(in) == 0){
+        fscanf(in, "%s", mess);
+        }
+    while (feof(diction) == 0){
+        fscanf(diction, "%s", d);
+        }
+        
+    if (strcmp(mess, d) == 0){
+    printf("Correct key");
+}
+
+        
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    
